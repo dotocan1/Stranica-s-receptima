c62e6144b1a77b381269519f92c8173928ad60d3
Dominik Otocan <dominik.otocan1@gmail.com>
2023-02-20 16:41:48 +0100

    .

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Stranica s receptima' });
 });
 
+/* GET recipe page with all recipes listed */
+router.get('/recipe', function(req, res, next) {
+  res.render('listRecipes');
+});
+
+/* GET grocery page with all groceries listed */
+router.get('/grocery', function(req, res, next) {
+  res.render('listGroceries');
+});
+
+/* GET category page with all categories listed */
+router.get('/category', function(req, res, next) {
+  res.render('listCategories');
+});
+
 module.exports = router;
