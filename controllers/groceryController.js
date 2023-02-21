39b0a959a4214b4855b99e85e13fc2dcb251b3db
Dominik Otocan <dominik.otocan1@gmail.com>
2023-02-21 18:39:09 +0100

    added a create a grocery page and made the code cleaner

---
+++ controllers/groceryController.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+const mongoose = require('mongoose');
+const Grocery = require('../models/groceryModel');
+
+// this lists all groceries
+exports.listAllGroceries = (req, res, next) => {
+    res.render('listGroceries');
+}
+
+// this renders the create a grocery page
+exports.createGroceryGet = (req, res, next) => {
+    res.render("createGroceryGet");
+}
+
+// this sends grocery data to the database
+exports.createGroceryPost = (req, res, next) => {
+
+}
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const groceryController = require("../controllers/groceryController");
+const recipeController = require("../controllers/recipeController");
+const categoryController = require("../controllers/categoryController");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Stranica s receptima' });
 });
 
-/* GET recipe page with all recipes listed */
-router.get('/recipe', function(req, res, next) {
-  res.render('listRecipes');
-});
+// GROCERY ROUTES
 
 /* GET grocery page with all groceries listed */
-router.get('/grocery', function(req, res, next) {
-  res.render('listGroceries');
-});
+router.get('/grocery', groceryController.listAllGroceries);
+
+// GET page for creating groceries
+router.get('/grocery/create', groceryController.createGroceryGet);
+
+// CATEGORY ROUTES
 
 /* GET category page with all categories listed */
-router.get('/category', function(req, res, next) {
-  res.render('listCategories');
-});
+// router.get('/category', groceryController.listAllCategories);
+
+// RECIPE ROUTES
+
+/* GET recipe page with all recipes listed */
+// router.get('/recipe', recipeController.listAllRecipes);
 
 module.exports = router;
