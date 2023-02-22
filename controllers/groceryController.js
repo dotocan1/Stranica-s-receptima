9a11d7f8ee2cce789958cd992cf7e926744b3137
Dominik Otocan <dominik.otocan1@gmail.com>
2023-02-22 11:15:18 +0100

    working on listing all groceries

--- controllers/groceryController.js
+++ controllers/groceryController.js
@@ -4,9 +4,22 @@ const mongoose = require('mongoose');
 const Grocery = require('../models/groceryModel');
 
 // this lists all groceries
-exports.listAllGroceries = (req, res, next) => {
-    res.render('listGroceries');
-}
+exports.listAllGroceries = async (req, res, next) => {
+
+    // TODO: code that lists all the groceries
+    // and then renders the page
+    // find all documents
+    await Grocery.find()
+        .sort([["name", "ascending"]])
+        .exec(function (err, listAllGroceries) {
+            if (err) {
+                return next(err);
+            };
+
+            // successful
+            res.render('listGroceries', { listAllGroceries: listAllGroceries });
+        })
+};
 
 // this renders the create a grocery page
 exports.createGroceryGet = (req, res, next) => {
