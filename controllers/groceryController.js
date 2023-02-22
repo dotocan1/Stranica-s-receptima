53d9b5fc4b8b6201ccc00b8ea466bdc85bf98da2
Dominik Otocan <dominik.otocan1@gmail.com>
2023-02-22 15:28:30 +0100

    listing items from groceries now works

--- controllers/groceryController.js
+++ controllers/groceryController.js
@@ -9,21 +9,14 @@ exports.listAllGroceries = async (req, res, next) => {
     // TODO: code that lists all the groceries
     // and then renders the page
     // find all documents
-    await Grocery.find()
-        .sort([["name", "ascending"]])
-        .exec(function (err, listAllGroceries) {
-            if (err) {
-                return next(err);
-            };
-
-            // successful
-            res.render('listGroceries', { listAllGroceries: listAllGroceries });
-        })
-};
+    const groceryArray = await Grocery.find()
+        .sort([["name", "ascending"]]);
+    res.render('listGroceries', {groceryArray: groceryArray});
+}
 
 // this renders the create a grocery page
 exports.createGroceryGet = (req, res, next) => {
-    res.render("createGroceryGet");
+    res.render("createGroceryGet", { arr: arr });
 }
 
 // this sends grocery data to the database
