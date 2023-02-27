2499a5e84a2688f792c41d27c558b1acc883414b
Dominik Otocan <dominik.otocan1@gmail.com>
2023-02-27 17:58:56 +0100

    .

--- public/scripts/createRecipeScript.js
+++ public/scripts/createRecipeScript.js
@@ -1,8 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log('hey')
-    document.querySelector('#btnAddMoreGroceries').addEventListener('click', () => {
-        console.log('Button works now');
-    })
-});
-
-
+console.log('hey');
+btn = document.querySelector('#btnAddMoreGroceries').addEventListener('click', () => {
+    console.log('Button works now');
+})
